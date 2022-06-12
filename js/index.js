eae0bd5adbc5c3aad0acff6506de0c03e395b186
Jino Hong <jinohong1993@gmail.com>
2022-06-11 21:36:09 -0300

    eu fiz o maximo que pude.. estudei bastante tambem , porem nao foi o suficiente para finaliza-lo cheguei a clonar array de htmlcollection para poder utilizar o metodo reduce, eu posso tentar finalizar ele depois ! porem por enquanto este eh o maximo

--- js/index.js
+++ js/index.js
@@ -2,23 +2,121 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  //   const price = product.querySelector('.price span');
+  // //... your code goes here
+  //   const qtt = product.querySelector('.quantity input')
+  //   const subtotal =price.innerText *qtt.innerText;
+  // return product.querySelector('.subtotal span') = subtotal
 
-  //... your code goes here
+  //   minha tentativa
+
+  // const price = document.getElementsByClassName('price span')
+  // const qtt = document.getElementsByClassName('quantity').value
+
+  const priceQuery = product.querySelector('.price span').innerHTML
+  const qttQuery = product.querySelector('.quantity input').value
+  const total = priceQuery*qttQuery
+  const subtt = product.querySelector('.subtotal span').innerHTML = total
+  return subtt;
 }
+
+// const price = document.getElementsByClassName('price')
+// const qtt = document.getElementsByClassName('quantity')
+// console.log(Number(price[0].getAttribute("span")[0])) //esse acho que funfou 
+// console.log(qtt)
+
+
+// const priceQuery = document.querySelector('.price span').innerHTML
+// const qttQuery = document.querySelector('.quantity input').value
+// const total = priceQuery*qttQuery
+// console.log(total)
+
+// console.log(priceQuery)
+// console.log(qttQuery)
+
+
+
+
+// const price = (document.querySelector('.price span'));
+// const qtt = (document.querySelector('.quantity input'));
+
+// // console.log(price.textContent)
+// console.log(price.textContent)
+// console.log(price.innerHTML)
+// console.log(price.innerText)
+// console.log(price.classList.value)
+
+// const qtt2 = document.getElementsByClassName("quantity")
+// console.log(qtt2)
+
+
+
+
+
+
+//   //... your code goes here
+//     const numPrice = price  
+//     // console.log(numPrice.textContent)
+
+
+
+
+
+
+
+//     const subtotal = price.textContent * qtt.textContent;
+
+// console.log(typeof price)
+// console.log(typeof qtt)
+//     // console.log(price)
+//     // console.log(qtt)
+//     console.log(subtotal)
+//     console.log(typeof subtotal)
+
+
+// const productArr = document.getElementsByClassName("product")
+// console.log(productArr)
+// // const singleProduct = document.querySelector('.product')
+// console.log(singleProduct)
+
+// const prodArr = document.getElementsByClassName("product")
+// console.log(prodArr)
+
+// const normalProdArr = [...prodArr]
+// console.log(normalProdArr)
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+  // // end of test
 
   // ITERATION 2
   //... your code goes here
+  const prodArr = document.getElementsByClassName("product");
+    for (i=0 ; i<=prodArr.length ; i++) {      
+      updateSubtotal(prodArr[i])
+      
+    }
+
 
   // ITERATION 3
   //... your code goes here
+
+    const totalDisplay = document.querySelector('#total-value span')
+    const normalProdArr = [...prodArr];
+    normalProdArr.reduce((a,b) => {
+      a+b;
+    })
+    totalDisplay =normalProdArr;
 }
+
+
+
+
+
 
 // ITERATION 4
 
